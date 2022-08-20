148ef55f023f02b868579c858c67533ee61b9f90
Boudjada Yasser <by.root96@gmail.com>
2022-08-20 19:34:11 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -38,11 +38,12 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
+
         "Programming Language :: Python :: 3 :: Only",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",

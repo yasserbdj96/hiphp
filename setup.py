221029179da3af28222fb50533a83731f48ed42b
Boudjada Yasser <by.root96@gmail.com>
2022-09-03 18:26:45 +0100

    0.2.25

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     version=__version__,
     author="yasserbdj96",
     author_email="yasser.bdj96@gmail.com",
-    description='''BackDoor to control php-based sites In the normal internet or .onion''',
+    description='''hiphp - free & open source project for create a BackDoor to control php-based sites In the normal internet or sites in The Onion Router (tor network).''',
     long_description_content_type="text/markdown",
     long_description=open('README.md','r', encoding="utf8").read(),
     license='''MIT License''',
@@ -32,7 +32,7 @@ setup(
         'WebSite':"https://yasserbdj96.github.io/"
     },
     install_requires=["requests","hexor","biglibrary"],
-    keywords=['yasserbdj96','python','hiphp','php','backdoor','http','https','onion','remoteaccess','controller','revisioncontrol'],
+    keywords=["windows","macos","linux","docker","cli","php","http","remoteaccess","backdoor","controller","https","hacking","python3","onion","termux","payload","website-hacking","yasserbdj96","revisioncontrol","hiphp"],
     classifiers=[
         "Environment :: Web Environment",
         "Development Status :: 5 - Production/Stable",

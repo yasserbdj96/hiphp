d2a0e82a4c4cb61c7939307857e0b9904d3907ca
Boudjada Yasser <by.root96@gmail.com>
2022-11-15 21:16:31 +0300

    clear lines
    clear lines
    
    Co-Authored-By: byroot96 <63003988+byroot96@users.noreply.github.com>
    Co-Authored-By: ressayb <112770731+ressayb@users.noreply.github.com>

--- hiphp/hiphplicense.py
+++ hiphp/hiphplicense.py
@@ -13,6 +13,7 @@
 
 #START{
 def license():
+
     license="""MIT License
 
                                  Apache License
@@ -217,4 +218,5 @@ def license():
    See the License for the specific language governing permissions and
    limitations under the License."""
     return license
+
 #}END.

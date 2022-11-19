bcaf18dfd1424d7f0378049dc66635c22dc3486d
Boudjada Yasser <by.root96@gmail.com>
2022-11-19 09:23:54 -0800

    Add files via upload
    Signed-off-by: Boudjada Yasser <by.root96@gmail.com>

--- install_all_requirements_linux.sh
+++ install_all_requirements_linux.sh
@@ -11,8 +11,10 @@
 #   |                                                          |
 
 #START{
-python3 -m pip install -r ./requirements.txt
-python3 -m pip install -r ./requirements-pypi.txt
-python3 -m pip install -r ./hiphp-desktop/requirements-dst.txt
-python3 -m pip install -r ./hiphp-tk/requirements-tk.txt
+python -m pip install -r ./requirements.txt
+python -m pip install -r ./requirements-pypi.txt
+python -m pip install -r ./hiphp-desktop/requirements-dst.txt
+python -m pip install -r ./hiphp-tk/requirements-tk.txt
+python -m pip install -r ./requirements-linux.txt
+
 #}END.

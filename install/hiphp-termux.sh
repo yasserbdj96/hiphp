d274732d2322650f76a329c60766d58d7c0b3a55
Boudjada Yasser <by.root96@gmail.com>
2023-05-26 15:22:08 +0100

    0.3.2
    Signed-off-by: Boudjada Yasser <by.root96@gmail.com>

--- install/hiphp-termux.sh
+++ install/hiphp-termux.sh
@@ -13,31 +13,78 @@
 #   |                                                         |   #
 
 #START{
-#help:
-if [ -z "$1" ]; then
-    python3 "${PREFIX}/share/hiphp/hiphplogo.py" "ok_view";
-    echo "Usage: hiphp [OPTION]";
-    echo "";
-    echo "Examples:";
-    echo "         hiphp --help                  # Show CLI help for hiphp.";
-    echo "         hiphp --geth [KEY] [URL]      # Retrieve the HIPHP_HOLE_CODE encrypted by your [KEY].";
-    echo "         hiphp [KEY] [URL]             # Connect to the victim's website in CLI mode.";
-    echo "         hiphp --version               # Check the current version number.";
+# Default values
+key=""
+url=""
+help=false
+dst=false
+version=false
+geth=false
+
+# Parse the command-line arguments
+for arg in "$@"; do
+  case "$arg" in
+    --key=*|--KEY=*)
+      key="${arg#*=}"
+      ;;
+    --url=*|--URL=*)
+      url="${arg#*=}"
+      ;;
+    --help|--HELP)
+      help=true
+      ;;
+    --dst|--DST)
+      DST=true
+      ;;
+    --version|--VERSION)
+      version=true
+      ;;
+    --geth|--GETH)
+      geth=true
+      ;;
+    *)
+      echo "Invalid argument: $arg" >&2
+      exit 1
+      ;;
+  esac
+done
 
 #cli help:
-elif [ "$1" == "--HELP" ] ; then
+if [ "$help" = true ]; then
+#elif [ "$1" == "--HELP" ] || [ "$1" == "--help" ]; then
     python3 "${PREFIX}/share/hiphp.py" --HELP;
 
+#hiphp_desktop:
+elif [ "$dst" = true ]; then
+#elif [ "$1" == "--DST" ] || [ "$1" == "--dst" ]; then
+    python3 "${PREFIX}/share/hiphp.py" --DST;
+
 #version:
-elif [ "$1" == "--VERSION" ] ; then
+elif [ "$version" = true ]; then
+#elif [ "$1" == "--VERSION" ] || [ "$1" == "--version" ]; then
     python3 "${PREFIX}/share/hiphp.py" --VERSION;
 
-#cli:
-else
-    if [ "$1" == "--GETH" ] ; then
+elif [ "$geth" = true ]; then
+    if [ -n "$key" ] || [ -n "$url" ]; then
         python3 "${PREFIX}/share/hiphp.py" --GETH --KEY=$2 --URL=$3;
     else
-        python3 "${PREFIX}/share/hiphp.py" --KEY=$1 --URL=$2;
+        echo "At least one variable is empty"
+    fi
+
+#help:
+else
+    if [ -n "$url" ] && [ -n "$key" ]; then
+        python3 "${PREFIX}/share/hiphp.py" --KEY=$key --URL=$url;
+    else
+        python3 "${PREFIX}/share/hiphp/hiphplogo.py" "ok_view";
+        echo "Usage: hiphp [OPTION]";
+        echo "";
+        echo "Examples:";
+        echo "         hiphp --help                             # Show CLI help for hiphp.";
+        echo "         hiphp --geth --key='[KEY]' --url='[URL]' # Retrieve the HIPHP_HOLE_CODE encrypted by your [KEY].";
+        echo "         hiphp --key='[KEY]' --url='[URL]'        # Connect to the victim's website in CLI mode.";
+        echo "         hiphp --dst                              # Run hiphp in 'hiphp-desktop' (GUI) mode.";
+        echo "         hiphp --version                          # Check the current version number.";
     fi
 fi
 #}END.
--- install/hiphp.sh
+++ install/hiphp.sh
@@ -15,41 +15,90 @@
 #START{
 cd /usr/share/hiphp/
 
-#help:
-if [ -z "$1" ]; then
-    python3 "/usr/share/hiphp/hiphp/hiphplogo.py" "ok_view";
-    echo "Usage: hiphp [OPTION]";
-    echo "";
-    echo "Examples:";
-    echo "         hiphp --help                  # Show CLI help for hiphp.";
-    echo "         hiphp --geth [KEY] [URL]      # Retrieve the HIPHP_HOLE_CODE encrypted by your [KEY].";
-    echo "         hiphp [KEY] [URL]             # Connect to the victim's website in CLI mode.";
-    echo "         hiphp --tk                    # Run hiphp in 'hiphp-tk' (GUI) mode.";
-    echo "         hiphp --dst                   # Run hiphp in 'hiphp-desktop' (GUI) mode.";
-    echo "         hiphp --version               # Check the current version number.";
+
+# Default values
+key=""
+url=""
+help=false
+tk=false
+dst=false
+version=false
+geth=false
+
+
+# Parse the command-line arguments
+for arg in "$@"; do
+  case "$arg" in
+    --key=*|--KEY=*)
+      key="${arg#*=}"
+      ;;
+    --url=*|--URL=*)
+      url="${arg#*=}"
+      ;;
+    --help|--HELP)
+      help=true
+      ;;
+    --tk|--TK)
+      tk=true
+      ;;
+    --dst|--DST)
+      DST=true
+      ;;
+    --version|--VERSION)
+      version=true
+      ;;
+    --geth|--GETH)
+      geth=true
+      ;;
+    *)
+      echo "Invalid argument: $arg" >&2
+      exit 1
+      ;;
+  esac
+done
 
 #cli help:
-elif [ "$1" == "--HELP" ] || [ "$1" == "--help" ]; then
+if [ "$help" = true ]; then
+#elif [ "$1" == "--HELP" ] || [ "$1" == "--help" ]; then
     python3 "/usr/share/hiphp/hiphp.py" --HELP;
 
 #hiphp_tk:
-elif [ "$1" == "--TK" ] || [ "$1" == "--tk" ]; then
+elif [ "$tk" = true ]; then
+#elif [ "$1" == "--TK" ] || [ "$1" == "--tk" ]; then
     python3 "/usr/share/hiphp/hiphp.py" --TK;
 
 #hiphp_desktop:
-elif [ "$1" == "--DST" ] || [ "$1" == "--dst" ]; then
+elif [ "$dst" = true ]; then
+#elif [ "$1" == "--DST" ] || [ "$1" == "--dst" ]; then
     python3 "/usr/share/hiphp/hiphp.py" --DST;
 
 #version:
-elif [ "$1" == "--VERSION" ] || [ "$1" == "--version" ]; then
+elif [ "$version" = true ]; then
+#elif [ "$1" == "--VERSION" ] || [ "$1" == "--version" ]; then
     python3 "/usr/share/hiphp/hiphp.py" --VERSION;
 
-#cli:
-else
-    if [ "$1" == "--GETH" ] || [ "$1" == "--geth" ]; then
+elif [ "$geth" = true ]; then
+    if [ -n "$key" ] || [ -n "$url" ]; then
         python3 "/usr/share/hiphp/hiphp.py" --GETH --KEY=$2 --URL=$3;
     else
-        python3 "/usr/share/hiphp/hiphp.py" --KEY=$1 --URL=$2;
+        echo "At least one variable is empty"
+    fi
+
+#help:
+else
+    if [ -n "$url" ] && [ -n "$key" ]; then
+        python3 "/usr/share/hiphp/hiphp.py" --KEY=$key --URL=$url;
+    else
+        python3 "/usr/share/hiphp/hiphp/hiphplogo.py" "ok_view";
+        echo "Usage: hiphp [OPTION]";
+        echo "";
+        echo "Examples:";
+        echo "         hiphp --help                             # Show CLI help for hiphp.";
+        echo "         hiphp --geth --key='[KEY]' --url='[URL]' # Retrieve the HIPHP_HOLE_CODE encrypted by your [KEY].";
+        echo "         hiphp --key='[KEY]' --url='[URL]'        # Connect to the victim's website in CLI mode.";
+        echo "         hiphp --tk                               # Run hiphp in 'hiphp-tk' (GUI) mode.";
+        echo "         hiphp --dst                              # Run hiphp in 'hiphp-desktop' (GUI) mode.";
+        echo "         hiphp --version                          # Check the current version number.";
     fi
 fi
 #}END.
